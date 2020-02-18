410b04285ffdee1b50779372beff71555df8e064
MinexAutomation <minex.automation@gmail.com>
2020-02-17 19:04:33 -0800

    Updated IServiceCollection extensions to use ServiceAction<T> functionality.

--- source/R5T.Norsica.Standard/Code/Extensions/IServiceCollectionExtensions.cs
+++ source/R5T.Norsica.Standard/Code/Extensions/IServiceCollectionExtensions.cs
@@ -4,6 +4,7 @@ using Microsoft.Extensions.DependencyInjection;
 
 using R5T.Caledonia;
 using R5T.Caledonia.Default;
+using R5T.Dacia;
 using R5T.Norsica.Configuration;
 using R5T.Norsica.Configuration.Sardinia;
 using R5T.Norsica.Default;
@@ -13,16 +14,46 @@ namespace R5T.Norsica.Standard
 {
     public static class IServiceCollectionExtensions
     {
+        /// <summary>
+        /// Adds the <see cref="IDotnetExecutableFilePathProvider"/> service.
+        /// </summary>
+        public static IServiceCollection AddDotnetExecutableFilePathProvider(this IServiceCollection services)
+        {
+            services.AddConfigurationBasedDotnetExecutableFilePathProvider(
+                services.AddDotnetConfigurationAction());
+
+            return services;
+        }
+
+        /// <summary>
+        /// Adds the <see cref="IDotnetExecutableFilePathProvider"/> service.
+        /// </summary>
+        public static ServiceAction<IDotnetExecutableFilePathProvider> AddDotnetExecutableFilePathProviderAction(this IServiceCollection services)
+        {
+            var serviceAction = new ServiceAction<IDotnetExecutableFilePathProvider>(() => services.AddDotnetExecutableFilePathProvider());
+            return serviceAction;
+        }
+
+        /// <summary>
+        /// Adds the <see cref="IDotnetOperator"/> service.
+        /// </summary>
         public static IServiceCollection AddDotnetOperator(this IServiceCollection services)
         {
-            services
-                .AddSingleton<IDotnetOperator, DotnetOperator>()
-                .AddSingleton<IDotnetExecutableFilePathProvider, ConfigurationBasedDotnetExecutableFilePathProvider>()
-                .AddDotnetConfiguration()
-                .AddSingleton<ICommandLineInvocationOperator, DefaultCommandLineInvocationOperator>()
+            services.AddDefaultDotnetOperator(
+                services.AddDefaultCommandLineInvocationOperatorAction(),
+                services.AddDotnetExecutableFilePathProviderAction())
                 ;
 
             return services;
+        }
+
+        /// <summary>
+        /// Adds the <see cref="IDotnetOperator"/> service.
+        /// </summary>
+        public static ServiceAction<IDotnetOperator> AddDotnetOperatorAction(this IServiceCollection services)
+        {
+            var serviceAction = new ServiceAction<IDotnetOperator>(() => services.AddDotnetOperator());
+            return serviceAction;
         }
     }
 }
